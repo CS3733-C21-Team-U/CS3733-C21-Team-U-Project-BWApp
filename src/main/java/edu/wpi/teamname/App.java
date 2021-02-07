24ecefa51c36674ea42faaefb1745681e61e0fa2
Kohmei Kadoya <kkadoya@wpi.edu>
2021-02-07 20:00:08 +0900

    Initial push for creating mutiple screens

--- src/main/java/edu/wpi/teamname/App.java
+++ src/main/java/edu/wpi/teamname/App.java
@@ -1,6 +1,9 @@
 package edu.wpi.teamname;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +16,17 @@ public class App extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {}
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("views/Kohmei.fxml"));
+
+    //
+    // root.getStylesheets().add(getClass().getResource("views/stylesheet.css").toExternalForm());
+
+    Scene scene = new Scene(root);
+
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
 
   @Override
   public void stop() {
---
+++ src/main/java/edu/wpi/teamname/views/MainPage.java
@@ -0,0 +1,48 @@
+package edu.wpi.teamname.views;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainPage {
+
+  @FXML private Button btnHome;
+
+  @FXML private Button btnKohmei;
+
+  @FXML
+  private void handleButtonAction(ActionEvent event) throws IOException {
+    Stage stage;
+    Parent root;
+
+    Object source = event.getSource();
+    if (btnHome == source) {
+
+      // get reference to the button's stage
+      stage = (Stage) btnHome.getScene().getWindow();
+      // load up OTHER FXML document
+      root = FXMLLoader.load(getClass().getResource("../views/MainPage.fxml"));
+
+    } else if (btnKohmei == source) {
+
+      // get reference to the button's stage
+      stage = (Stage) btnKohmei.getScene().getWindow();
+      // load up OTHER FXML document
+      root = FXMLLoader.load(getClass().getResource("../views/Kohmei.fxml"));
+
+    } else { // code block
+      System.out.println("Switch for button handler had a non-exsistent source");
+      return;
+    }
+
+    // create a new scene with root and set the stage
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+}
