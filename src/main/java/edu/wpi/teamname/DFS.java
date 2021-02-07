28e24ac0b5475bf4f444f367990fe1fc39d4e61a
gearymich <Founders301_>
2021-02-07 16:31:12 -0500

    Directional DFS 2nd
    Added Printlns and stuff

--- src/main/java/edu/wpi/teamname/DFS.java
+++ src/main/java/edu/wpi/teamname/DFS.java
@@ -1,8 +1,10 @@
 package edu.wpi.teamname;
+
 import java.util.Hashtable;
 
 public class DFS {
-  public Hashtable<node, Boolean> marked; // passes in node as key, returns true if node is marked, null otherwise
+  public Hashtable<node, Boolean>
+      marked; // passes in node as key, returns true if node is marked, null otherwise
 
   public DFS(node s) {
     marked = new Hashtable<node, Boolean>();
@@ -11,7 +13,8 @@ public class DFS {
 
   // depth first search from node s
   private void dfs(node s) {
-    marked.put(s, true); //marks current node as visited
+    marked.put(s, true); // marks current node as visited
+    System.out.print(s.nodeID + " ");
     for (edge w : s.edges) { // for every edge beginning from node s (source)
       if (marked.get(w.endNode) == null) { // if node has not been reached yet, search it
         dfs(w.endNode);
--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -10,17 +10,16 @@ public class Main {
     node E = new node(5, -0.5, 1.5);
     node F = new node(6, -1, 2);
 
-    edge G = new edge(1, A, D);
-    edge H = new edge(2, B, E);
-    edge I = new edge(3, C, E);
-    edge J = new edge(4, E, F);
-    edge K = new edge(5, C, F);
-    edge L = new edge(6, F, C);
+    edge G = new edge(1, A, B);
+    edge H = new edge(2, B, C);
+    edge I = new edge(3, D, C);
+    edge J = new edge(4, D, E);
+    edge K = new edge(5, E, F);
+    edge L = new edge(6, F, A);
 
-    DFS search = new DFS(E); // starting node
-
-    if (search.marked.get(C) != null) System.out.println("C is connected");
-    else System.out.println("NOT connected");
+    DFS search = new DFS(B); // starting node
+    if (search.marked.get(F) != null) System.out.println("\nC is connected");
+    else System.out.println("\nNOT connected");
     App.launch(App.class, args);
   }
 }
