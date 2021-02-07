e3b6ea39a1258f2fc2c08c3d8a5ac71f0f50684e
Kohmei Kadoya <kkadoya@wpi.edu>
2021-02-07 20:45:57 +0900

    trying to stop the test failed email

--- src/test/java/edu/wpi/teamname/DefaultTest.java
+++ src/test/java/edu/wpi/teamname/DefaultTest.java
@@ -6,8 +6,12 @@ package edu.wpi.teamname;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DefaultTest {
 
   @Test
-  public void test() {}
+  public void test() {
+    assertEquals(2, 2);
+  }
 }
