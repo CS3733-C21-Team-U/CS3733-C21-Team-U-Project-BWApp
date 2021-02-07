647153ff8713093184b0ec495f982d48107ef8fa
gearymich <Founders301_>
2021-02-07 16:46:09 -0500

    Bi-Directional DFS
    Made the edges of the graph point in both directions

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -2,6 +2,11 @@ package edu.wpi.teamname;
 
 public class Main {
 
+  public static void makeEdge(int _edgeID, node _startNode, node _endNode) {
+    new edge(_edgeID, _startNode, _endNode);
+    new edge(-_edgeID, _endNode, _startNode);
+  }
+
   public static void main(String[] args) {
     node A = new node(1, 0, 0);
     node B = new node(2, 2, 0);
@@ -10,12 +15,12 @@ public class Main {
     node E = new node(5, -0.5, 1.5);
     node F = new node(6, -1, 2);
 
-    edge G = new edge(1, A, B);
-    edge H = new edge(2, B, C);
-    edge I = new edge(3, D, C);
-    edge J = new edge(4, D, E);
-    edge K = new edge(5, E, F);
-    edge L = new edge(6, F, A);
+    makeEdge(1, A, B);
+    makeEdge(2, B, C);
+    makeEdge(3, D, C);
+    makeEdge(4, D, E);
+    makeEdge(5, E, F);
+    makeEdge(6, F, A);
 
     DFS search = new DFS(B); // starting node
     if (search.marked.get(F) != null) System.out.println("\nC is connected");
