beefdce11f9d2df6107449c4fec4f36eaf86d7db
cvkittler <cvkittler@wpi.edu>
2021-02-06 19:05:52 -0500

    switched weighted Graph to an adjacentcy Matrix

--- src/main/java/edu/wpi/teamname/weightedGraph.java
+++ src/main/java/edu/wpi/teamname/weightedGraph.java
@@ -1,60 +1,33 @@
 package edu.wpi.teamname;
 
-import java.util.LinkedList;
-
 public class weightedGraph {
-  int numEdges = 0;
-  LinkedList<edge>[] adjacencyList;
+  int numEdges;
+  int numNodes;
+  double[][] adjacencyMatrix;
 
-  weightedGraph(edge edgeList[]) {
+  weightedGraph(node[] nodeList, edge[] edgeList) {
+    this.numNodes = nodeList.length;
     this.numEdges = edgeList.length;
-    adjacencyList = new LinkedList[this.numEdges];
-    for (int i = 0; i < this.numEdges; i++) {
-      this.adjacencyList[i] = new LinkedList<>();
-    }
+
+    adjacencyMatrix = new double[numNodes][numNodes];
+
     for (int i = 0; i < numEdges; i++) {
-      edge currentEdge = edgeList[i];
-      for (int j = 0; j < numEdges; j++) {
-        edge comparedEdge = edgeList[j];
+      this.adjacencyMatrix[edgeList[i].startNode.nodeID][edgeList[i].endNode.nodeID] =
+          edgeList[i].weight;
+      this.adjacencyMatrix[edgeList[i].endNode.nodeID][edgeList[i].startNode.nodeID] =
+          edgeList[i].weight;
+    }
+  }
 
-        if (currentEdge.endNode == comparedEdge.startNode) { // if the edges are connected
-          this.adjacencyList[currentEdge.endNode.nodeID].addFirst(comparedEdge);
-        }
+  public void print() {
+    for (int i = 0; i < this.numNodes; i++) {
+      for (int j = 0; j < this.numNodes; j++) {
+        double A = this.adjacencyMatrix[i][j] * 100;
+        double B = Math.round(A);
+        double C = B / 100;
+        System.out.print(C + " ");
       }
+      System.out.print("\n");
     }
   }
-
-  public void printGraph() {
-    for (int i = 0; i < this.numEdges; i++) {
-      LinkedList<edge> list = adjacencyList[i];
-      for (int j = 0; j < list.size(); j++) {
-        System.out.println(
-            "Node[ID: "
-                + i
-                + "] is connected to Node[ID: "
-                + list.get(j).endNode.nodeID
-                + "] and is "
-                + list.get(j).weight
-                + " units");
-      }
-    }
-  }
-
-  public node[] adj(node _curNode){
-    LinkedList<edge> list = adjacencyList[_curNode.nodeID];
-    node[] returnMe = new node [list.size()];
-
-    for(int i = 0; i  < list.size(); i++){
-      returnMe[i] = list.get(i).endNode;
-    }
-
-    return  returnMe;
-  }
-
-  public node[] listAllNodes(){
-    node[] returnMe = new node[];
-
-
-  }
-
 }
