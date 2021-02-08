8c264eedd4f2486dfad0cafd4576f750a2ff9849
Neville Ingram <nlingram@wpi.edu>
2021-02-08 16:46:32 -0500

    Added command line parsing & part d

--- src/main/java/edu/wpi/teamname/UDB.java
+++ src/main/java/edu/wpi/teamname/UDB.java
@@ -1,40 +1,67 @@
 package edu.wpi.teamname;
 
+import static java.lang.System.exit;
+
 import java.sql.*;
+import java.util.Scanner;
 
 public class UDB {
 
-  private Connection conn = null; // used for connection
-  private Statement stmt; // used for sql statements
-  private ResultSet rset; // used to store returns from sql queries
-  private String url =
+  private static Connection conn = null; // used for connection
+  private static Statement stmt; // used for sql statements
+  private static ResultSet rset; // used to store returns from sql queries
+  private static String url =
       "jdbc:derby:UDB;create=true;user=admin;password=admin"; // link of embedded database
   private static int option; // used to know what function to run
 
-  private int flag; // flag for detecting if first time running 
+  private static int flag = 0; // flag for detecting if first time running
 
   public static void main(String args[]) {
     // Input logic
+    Scanner s = new Scanner(System.in);
+
+    if (args.length < 5 && flag == 0) {
+      System.out.println(
+          "1-Report Museum Information\n"
+              + "2-Report Paintings in Museums\n"
+              + "3-Update Museum Phone Number"
+              + "4-Exit Program");
+      driver();
+      connect();
+      init();
+      insertData();
+    } else {
+      option = s.nextInt();
+      switch (option) {
+        case 1:
+          printMuseums();
+          break;
+        case 2:
+          printByName();
+          break;
+        case 3:
+          updatePhoneNumber();
+          break;
+        case 4:
+          stop();
+          break;
+        default:
+          System.out.println(
+              "1-Report Museum Information\n"
+                  + "2-Report Paintings in Museums\n"
+                  + "3-Update Museum Phone Number"
+                  + "4-Exit Program");
+          break;
+      }
+    }
   }
 
-  public UDB() {
-    /*
-    Always run driver() & connect().
-    init(), insertData() only run on creation
-    stop() only run on exit (option 4)
-    All other functions are options based
-     */
-    driver();
-    connect();
-    // createAdmin();
-    // init();
-    // insertData();
-    // printMuseums();
-    // printByName();
-    // stop();
-  }
+  public UDB() {}
 
-  public void driver() {
+  /*
+  Runs everytime
+   */
+  public static void driver() {
     try {
       Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
       // registers the driver for the database
@@ -44,8 +71,10 @@ public class UDB {
     }
     System.out.println("Driver Registered & Database Created!");
   }
-
-  public void connect() {
+  /*
+  Runs everytime
+   */
+  public static void connect() {
     try {
       conn = DriverManager.getConnection(url);
       // establishes a connection to the database
@@ -55,38 +84,44 @@ public class UDB {
     }
     System.out.println("Connection succeeded");
   }
-
-  public void createAdmin() {
+  /*
+  Runs at start
+   */
+  public static void createAdmin() {
     try {
-      stmt = conn.createStatement();
       String str = "call syscs_util.syscs_create_user('admin', 'admin')";
-      stmt.execute(str);
+      PreparedStatement ps = conn.prepareStatement(str);
+      ps.execute();
     } catch (Exception e) {
       System.out.println("Failed to create user");
       e.printStackTrace();
     }
   }
-
-  public void init() {
+  /*
+  Runs at start
+   */
+  public static void init() {
     try {
-      Statement stmt = conn.createStatement(); // creates statement
       String tbl1 =
           "create table Museums (id int not null generated always as identity, paintings int not null, name varchar(50), phone varchar(50), primary key (id))";
       // code for creating table of Museums
-      stmt.execute(tbl1); // executes the sql in tbl1 string
+      PreparedStatement ps1 = conn.prepareStatement(tbl1);
+      ps1.execute();
       String tbl2 =
           "create table Paintings (id int not null generated always as identity, name varchar(50), artist varchar(50), museum int not null references Museums, primary key(id))";
-      stmt.execute(tbl2);
+      PreparedStatement ps2 = conn.prepareStatement(tbl2);
+      ps2.execute();
     } catch (Exception e) {
       System.out.println("Table creation failed");
       e.printStackTrace();
     }
-    System.out.println("Table creation succeeded!");
   }
-
-  public void insertData() {
+  /*
+  Runs at start
+   */
+  public static void insertData() {
     try {
-      Statement stmt = conn.createStatement();
+
       String str =
           "insert into Museums("
               + "paintings, name, phone) values "
@@ -95,7 +130,8 @@ public class UDB {
               + "(2, 'British Museum', '4600'), "
               + "(5, 'Metropolitan', '911'), "
               + "(3, 'Vatican', '90909')";
-      stmt.execute(str);
+      PreparedStatement ps = conn.prepareStatement(str);
+      ps.execute();
       str =
           "insert into Paintings("
               + "name, artist, museum) values "
@@ -119,20 +155,21 @@ public class UDB {
               + "('Painting 18', 'Artist 18', 3),"
               + "('Painting 19', 'Artist 19', 4),"
               + "('Painting 20', 'Artist 20', 5)";
-      stmt.execute(str);
+      PreparedStatement ps2 = conn.prepareStatement(str);
+      ps2.execute();
     } catch (Exception e) {
       System.out.println("Failed to insert into table");
       e.printStackTrace();
     }
-    System.out.println("Successfully inserted into database!");
   }
-
-  public void printMuseums() {
+  /*
+  Only run when given the command of '1'
+   */
+  public static void printMuseums() {
     try {
-      stmt = conn.createStatement();
       String str = "select * from Museums";
-      // gets all column data from Museums
-      rset = stmt.executeQuery(str);
+      PreparedStatement ps = conn.prepareStatement(str);
+      rset = ps.executeQuery();
       // store the data inside of a ResultSet object
       while (rset.next()) { // iterates through the object row by row
         int id = rset.getInt("id"); // gets value at column id
@@ -145,15 +182,17 @@ public class UDB {
       System.out.println("Failed to select from Museums");
     }
   }
-
-  public void printByName() {
+  /*
+  Only run when given the command of '2'
+   */
+  public static void printByName() {
     // prints name of each museum followed by the paintings in them
     try {
       String str2;
       ResultSet rset2;
-      stmt = conn.createStatement();
       String str = "select * from Paintings"; // we want all paintings info
-      rset = stmt.executeQuery(str);
+      PreparedStatement ps1 = conn.prepareStatement(str);
+      rset = ps1.executeQuery();
       while (rset.next()) {
         int museum_id = rset.getInt("museum"); // get the id from the museum
         String name = rset.getString("name");
@@ -168,9 +207,9 @@ public class UDB {
           System.out.println(
               "Museum: "
                   + rset2.getString("name")
-                  + " Name of Painting: "
+                  + "| Name of Painting: "
                   + name
-                  + " Artist: "
+                  + "| Artist: "
                   + artist);
         }
         rset2.next();
@@ -182,10 +221,16 @@ public class UDB {
   }
 
   /*
-  Needs to be written
+  Only run when given the command of '3'
   */
-  public void updatePhoneNumber(String museum, int new_phone_number) {
+  public static void updatePhoneNumber() {
     try {
+      Scanner s = new Scanner(System.in);
+      System.out.println("Please enter the museum name (Case sensitive)");
+      String museum = s.nextLine();
+      System.out.println("Please enter a new phone number");
+      int new_phone_number = Integer.getInteger(s.nextLine());
+
       String str = "update Museums set phone=? where name=?";
       PreparedStatement ps = conn.prepareStatement(str);
       ps.setInt(1, new_phone_number);
@@ -199,7 +244,7 @@ public class UDB {
   /*
   Only run when given the stop command of '4'
    */
-  public void stop() {
+  public static void stop() {
     try {
       stmt = conn.createStatement();
       String str = "alter table Paintings drop column museum"; // drops the foreign key
@@ -215,5 +260,6 @@ public class UDB {
       e.printStackTrace();
     }
     System.out.println("Tables dropped & STMT, & CONN closed!");
+    exit(0);
   }
 }
