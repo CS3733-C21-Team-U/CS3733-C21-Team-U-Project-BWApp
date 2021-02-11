933ebac0390851945179fcd9890dee653a6e1cbc
cvkittler <cvkittler@wpi.edu>
2021-02-11 14:45:05 -0500

    Commented the A* code
    added comments to the A* code so it is easy to understand

--- src/main/java/edu/wpi/teamname/algorithms/AStar.java
+++ src/main/java/edu/wpi/teamname/algorithms/AStar.java
@@ -7,48 +7,60 @@ import java.util.Set;
 public class AStar {
   private Graph map;
 
-  public AStar(Graph graph) { // initial AStar
+  public AStar(Graph graph) { // initial map for AStar
     this.map = graph;
   }
 
+  public void updateAStar(Graph graph){ // for updating map
+    this.map = graph;
+  }
   public void run(node _startNode, node _goalNode) {
-    Set<node> queue = new HashSet<>();
-    queue.add(_startNode);
-    HashMap<node, node> cameFrom = new HashMap<>();
-    HashMap<node, Double> cost = new HashMap<>();
-    HashMap<node, Double> priority = new HashMap<>();
+    Set<node> reachableNodes = new HashSet<>();  //record the reachable but unvisited nodes
+    reachableNodes.add(_startNode);              //add the start node to the seen nodes
+    HashMap<node, node> cameFrom = new HashMap<>();   //record what node leads to which for shortest path
+    HashMap<node, Double> cost = new HashMap<>();     //record the cost to get to each node from the start node
+    HashMap<node, Double> priority = new HashMap<>(); //priority the cost + euclidean distance to the goal
+
+    //setup the start node in the data
     cost.put(_startNode, 0.0);
     priority.put(_startNode, 0.0);
     cameFrom.put(_startNode, _startNode);
 
+    //create the variable used in A*
     node current = _startNode;
-    double next_cost;
 
-    while (!queue.isEmpty()) {
-      current = getNextNode(queue, priority);
+    //while there is reachable nodes loop
+    while (!reachableNodes.isEmpty()) {
+      //get the next node
+      current = getNextNode(reachableNodes, priority);
 
       if (current == _goalNode) {
         // found the end
         break;
       }
-
-      for (node next : current.adjNodes()) {
-        next_cost = cost.get(current) + distBetweenNodes(current, next);
-        if (!cost.containsKey(next)) {
-          cost.put(next, next_cost);
-          priority.put(next, next_cost + distBetweenNodes(next, _goalNode));
-          queue.add(next);
-          cameFrom.put(next, current);
-        } else if (cost.get(next) > next_cost) {
-          cost.put(next, next_cost);
-          priority.put(next, next_cost + distBetweenNodes(next, _goalNode));
-          queue.add(next);
-          cameFrom.put(next, current);
+      //iterate through the linked list of adjacent nodes
+      for (node adjNode : current.adjNodes()) {
+        //calculate the cost to get to get from the start to the adjacent node
+        double nextNodeCost = cost.get(current) + distBetweenNodes(current, adjNode);
+        //if the adjacent node has not been seen yet add it
+        if (!cost.containsKey(adjNode)) {
+          cost.put(adjNode, nextNodeCost);
+          priority.put(adjNode, nextNodeCost + distBetweenNodes(adjNode, _goalNode));
+          reachableNodes.add(adjNode);
+          cameFrom.put(adjNode, current);
+        }
+        //if the cost to get to the adjacent node is less update its cost and add it back to the reachable nodes
+        else if (cost.get(adjNode) > nextNodeCost) {//this has to be in the else if because cost.containsKey can return NULL
+          cost.put(adjNode, nextNodeCost);
+          priority.put(adjNode, nextNodeCost + distBetweenNodes(adjNode, _goalNode));
+          reachableNodes.add(adjNode);
+          cameFrom.put(adjNode, current);
         }
       }
     } // end of while
 
-    if (cameFrom.containsKey(_goalNode)) { // exited with a path
+    //print out the path if there is one
+    if (cameFrom.containsKey(_goalNode)) { //if exited with a path
       System.out.print("[END] ");
       while (current != _startNode) {
         System.out.print("[Node " + current.nodeID + "] <- ");
@@ -58,6 +70,7 @@ public class AStar {
     }
   }
 
+  //takes in two nodes and returns the distance between them
   private double distBetweenNodes(node n1, node n2) {
     // calculate the rise and run
     double dx = Math.abs(n1.xcoord - n2.xcoord);
@@ -66,18 +79,22 @@ public class AStar {
     return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
   }
 
-  private node getNextNode(Set<node> _queue, HashMap<node, Double> priority) {
-    node[] nodes = new node[_queue.size()];
-    _queue.toArray(nodes);
-
+  //returns the node with the lowest priority from the set of reachable nodes
+  private node getNextNode(Set<node> _reachableNodes, HashMap<node, Double> priority) {
+    //convert the set of reachable nodes into an array
+    node[] nodes = new node[_reachableNodes.size()];
+    _reachableNodes.toArray(nodes);
+    //initialize a first node for comparison
     node returnNode = nodes[0];
-
+    //iterate through the array and find the node with the lowest priority
     for (node curNode : nodes) {
       if (priority.get(returnNode) > priority.get(curNode)) {
         returnNode = curNode;
       }
     }
-    _queue.remove(returnNode);
+    //take the node picked out of the set of reachable nodes
+    _reachableNodes.remove(returnNode);
+    //return the node
     return returnNode;
   }
 }
