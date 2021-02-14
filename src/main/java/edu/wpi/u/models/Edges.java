c466eb828fde721fbf14822852a1c060b5120598
Neville Ingram <nlingram@wpi.edu>
2021-02-14 17:31:38 -0500

    Reformat models

---
+++ src/main/java/edu/wpi/u/models/Edges.java
@@ -0,0 +1,18 @@
+package edu.wpi.u.models;
+
+public class Edges {
+    public Edges (){}
+    public int addEdge(String start_node, String end_node){
+        return 1;
+    }
+    public int updEdgeStart(String id, String new_start_node){
+        return 1;
+    }
+    public int updEdgeEnd(String id, String new_end_node){
+        return 1;
+    }
+    public int delEdge(String id){
+        return 1;
+    }
+
+}
