2b0701691304084d6f899a975e39852d6eb418ae
Olajumoke <olajumoke.jackson@gmail.com>
2021-02-14 19:21:13 -0500

    code includes ability to load and save csv

--- src/main/java/edu/wpi/teamname/BWdb.java
+++ src/main/java/edu/wpi/teamname/BWdb.java
@@ -1,6 +1,10 @@
 package edu.wpi.teamname;
 
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.Scanner;
 import java.util.*;
@@ -16,15 +20,17 @@ public class BWdb {
   private static int option; // used to know what function to run
 
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
     BWdb db = new BWdb();
   }
 
-  public BWdb() {
+  public BWdb() throws SQLException {
     driver();
     connect();
     init();
-    insertData();
+    dropValues();
+    insertNodeData();
+    insertEdgeData();
     printNodes();
     // stop();
   }
@@ -71,27 +77,81 @@ public class BWdb {
     }
   }
 
-  public static void insertData() {
-    try {
-        // TODO: fix this statement
-      // String str = ""; // COMMENT OUT WHEN UNCOMMENTING BELOW
+  public static void dropValues() {
+    try{
+      String str = "delete from Nodes";
+      PreparedStatement ps = conn.prepareStatement(str);
+      ps.execute();
+      str = "delete from Edges";
+      ps.execute();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+  }
 
+  public static void insertNodeData() throws SQLException {
+    String str ="";
 
-      String str =
-            "call SYSCS_UTIL.SYSCS_IMPORT_TABLE_BULK"
-        + " (null, 'NODES', 'src/main/resources/edu/wpi/teamname/OutsideMapNodes.csv', ',', null, null, 0, 1)";
+    try  {
+      BufferedReader br = Files.newBufferedReader(Paths.get("OutsideMapNodes.csv"));
 
+      String DELIMITER = ",";
 
-
+      // read the file line by line
+      String line;
+      while ((line = br.readLine()) != null) {
+        str = "insert into Nodes (nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName, teamAssigned) values (?,?,?,?,?,?,?,?,?)";
         PreparedStatement ps = conn.prepareStatement(str);
-        ps.execute();
-        str =
-            "insert into Edges("
-                + "edgeID, startID, endID) values "
-                + "('U1234_U5678', 'U1234', 'U5678')";
-        PreparedStatement ps2 = conn.prepareStatement(str);
-        ps2.execute();
 
+        // convert line into columns
+        String[] columns = line.split(DELIMITER);
+        ps.setString(1, columns[0]);
+        ps.setInt(2, Integer.parseInt(columns[1]));
+        ps.setInt(3, Integer.parseInt(columns[2]));
+        ps.setInt(4, Integer.parseInt(columns[3]));
+        ps.setString(5, columns[4]);
+        ps.setString(6, columns[5]);
+        ps.setString(7, columns[6]);
+        ps.setString(8, columns[7]);
+        ps.setString(9, columns[8]);
+        ps.execute();
+
+
+        // print all columns
+        //System.out.println("User["+ String.join(", ", columns) +"]");
+      }
+
+    } catch (Exception e) {
+      System.out.println("Failed to insert into table");
+      e.printStackTrace();
+    }
+  }
+
+  public static void insertEdgeData() throws SQLException {
+    String str ="";
+
+    try  {
+      BufferedReader br = Files.newBufferedReader(Paths.get("OutsideMapEdges.csv"));
+
+      String DELIMITER = ",";
+
+      // read the file line by line
+      String line;
+      while ((line = br.readLine()) != null) {
+        str = "insert into Edges (edgeID, startID, endID) values (?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(str);
+
+        // convert line into columns
+        String[] columns = line.split(DELIMITER);
+        ps.setString(1, columns[0]);
+        ps.setString(2, columns[1]);
+        ps.setString(3, columns[2]);
+        ps.execute();
+
+
+        // print all columns
+        //System.out.println("User["+ String.join(", ", columns) +"]");
+      }
 
     } catch (Exception e) {
       System.out.println("Failed to insert into table");
@@ -106,13 +166,96 @@ public class BWdb {
       rset = ps.executeQuery();
       // store the data inside of a ResultSet object
       while (rset.next()) { // iterates through the object row by row
-        String nodeID = rset.getString("nodeID"); // gets value at column name
-        System.out.println("ID:" + nodeID + "\n");
+        String nodeID = rset.getString("nodeID");
+        int xcoord = rset.getInt("xcoord");// gets value at column name
+        System.out.println("ID:" + nodeID + " " + "xcoord:" + xcoord + "\n");
       }
       rset.close(); // close the object
     } catch (Exception e) {
       System.out.println("Failed to select from Museums");
       e.printStackTrace();
     }
+  }
+
+  public static void saveNodesCSV() {
+    try {
+      String str = "SELECT * FROM Nodes";
+
+      Statement statement = conn.createStatement();
+
+      ResultSet result = statement.executeQuery(str);
+
+      BufferedWriter fileWriter = new BufferedWriter(new FileWriter("OutsideMapNodes.csv"));
+
+      // write header line containing column names
+      fileWriter.write("nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName, teamAssigned");
+
+      while (result.next()) {
+        String nodeID = result.getString("nodeID");
+        int xcoord = result.getInt("xcoord");
+        int ycoord = result.getInt("ycoord");
+        int floor = result.getInt("floor");
+        String building = result.getString("building");
+        String nodeType = result.getString("nodeType");
+        String longName = result.getString("longName");
+        String shortName = result.getString("shortName");
+        String teamAssigned = result.getString("teamAssigned");
+
+        String line = String.format("\"%s\",%s,%.1f,%s,%s",
+                nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName, teamAssigned);
+
+        fileWriter.newLine();
+        fileWriter.write(line);
+      }
+
+      statement.close();
+      fileWriter.close();
+
+    } catch (SQLException e) {
+      System.out.println("Datababse error:");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("File IO error:");
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void saveEdgesCSV() {
+    try {
+      String str = "SELECT * FROM Edges";
+
+      Statement statement = conn.createStatement();
+
+      ResultSet result = statement.executeQuery(str);
+
+      BufferedWriter fileWriter = new BufferedWriter(new FileWriter("OutsideMapNodes.csv"));
+
+      // write header line containing column names
+      fileWriter.write("edgeID, startID, endID");
+
+      while (result.next()) {
+        String edgeID = result.getString("edgeID");
+        String startID = result.getString("startID");
+        String endID = result.getString("endID");
+
+        String line = String.format("\"%s\",%s,%.1f,%s,%s",
+                edgeID, startID, endID);
+
+        fileWriter.newLine();
+        fileWriter.write(line);
+      }
+
+      statement.close();
+      fileWriter.close();
+
+    } catch (SQLException e) {
+      System.out.println("Datababse error:");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("File IO error:");
+      e.printStackTrace();
+    }
+
   }
 }
