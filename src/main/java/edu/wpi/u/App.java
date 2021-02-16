0d1d3dab1d444ac991bfcc56a2dce7dc06d427b4
Lily Durkin <lzdurkin@wpi.edu>
2021-02-16 01:24:58 -0500

    Added more functionality to NodeController and added global access to App's graph and other potential future stored data.

--- src/main/java/edu/wpi/u/App.java
+++ src/main/java/edu/wpi/u/App.java
@@ -1,6 +1,8 @@
 package edu.wpi.u;
 
+import edu.wpi.u.algorithms.Node;
 import edu.wpi.u.models.Graph;
+import edu.wpi.u.models.Message;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,11 +12,39 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App extends Application {
+  //Allows for globally accessible instance of app to allow instance based editing (separate info per open application)
+  //Can be accessed by all controllers and classes by calling App.getInstance();
+  private static final App instance = new App();
+  public Graph graph = new Graph();
 
-  public static Graph graph = new Graph();
+  //Gets the current instance of App
+  public static App getInstance() {
+    return instance;
+  }
+
+  public App() {
+    Node A = new Node("A", 0, 0);
+    Node B = new Node("B", 1, 0);
+    Node C = new Node("C", 2, 0);
+    Node D = new Node("D", 2, 1);
+    Node E = new Node("E", 1, 30);
+    Node F = new Node("F", 0, 1);
+
+    graph.addEdge("1", "A", "B");
+    graph.addEdge("2", "B", "C");
+    graph.addEdge("3", "D", "C");
+    graph.addEdge("4", "D", "E");
+    graph.addEdge("5", "E", "F");
+    graph.addEdge("6", "F", "A");
+    graph.addEdge("7", "F", "B");
+    graph.addEdge("8", "E", "A");
+    graph.addEdge("9", "B", "D");
+  }
+
+  public static Message savedData = new Message();
 
   private static Stage primaryStage; // This is a static variable!!
-  // We only ever have one primary stage, each time we switch scenes, we swap the root out
+  // We only ever have one primary stage, each time we switch scenes, we swap this out
 
   public static Stage getPrimaryStage() {
     return primaryStage;
--- src/main/java/edu/wpi/u/controllers/NodeController.java
+++ src/main/java/edu/wpi/u/controllers/NodeController.java
@@ -1,9 +1,12 @@
 package edu.wpi.u.controllers;
 
 import edu.wpi.u.App;
+import edu.wpi.u.algorithms.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class NodeController {
@@ -14,45 +17,123 @@ public class NodeController {
 
   @FXML public TextField enterYCoo;
 
-  @FXML
-  public void addNode() {
+  @FXML public Label errorLabel;
 
-    if (enterNodeID.getText().equals("")) {
-      System.out.println("Missing Node ID.");
-      return;
+  @FXML public Button editNodeButton;
+
+  @FXML public Button deleteNodeButton;
+
+  public void initialize() {
+    update();
+  }
+
+  public void update() {
+    // Uncomment when graph implements empty check or properly implements getNodes()
+    // If getNodes() gets functionality first then put
+    //
+    // App.getInstance().graph.getNodes().isEmpty() into if statement below
+    /*
+    if (App.getInstance().graph.isEmpty()) {
+      editNodeButton.setDisable(true);
+      deleteNodeButton.setDisable(true);
+    } else {
+      editNodeButton.setDisable(false);
+      deleteNodeButton.setDisable(false);
     }
-
-    if (enterXCoo.getText().equals("")) {
-      System.out.println("Missing x-coordinate.");
-      return;
-    }
-
-    if (enterYCoo.getText().equals("")) {
-      System.out.println("Missing y-coordinate");
-      return;
-    }
-
-    //        Node myNode = new Node(enterNodeID.getText(),);
-
+     */
   }
 
   @FXML
-  public void editNode() {}
+  public void addNode() {
+
+    String tempID = enterNodeID.getText();
+
+    if (tempID.equals("")) {
+      errorLabel.setText("Missing Node ID.");
+      return;
+    }
+    if (checkTextBoxesErrorCoordiantes()) return;
+
+    int tempX = Integer.parseInt(enterXCoo.getText());
+    int tempY = Integer.parseInt(enterYCoo.getText());
+
+    String ret = App.getInstance().graph.addNode(tempID, tempX, tempY);
+    if (ret.equals(tempID)) {
+      errorLabel.setText("Node already exists");
+      return;
+    }
+
+    update();
+    errorLabel.setText("Node added successfully.");
+  }
 
   @FXML
-  public void deleteNode() {}
+  public void editNode() {
+    String tempID = enterNodeID.getText();
+    if (tempID.equals("")) {
+      errorLabel.setText("Missing Node ID.");
+      return;
+    }
+    if (checkTextBoxesErrorCoordiantesEmpty()) return;
+    if (checkTextBoxesErrorCoordiantes()) return;
+    int tempX = Integer.parseInt(enterXCoo.getText());
+    int tempY = Integer.parseInt(enterYCoo.getText());
+    if (App.getInstance().graph.updateNode(tempID, tempX, tempY).equals(tempID))
+      errorLabel.setText("Node does not exists.");
+    else{
+      update();
+      errorLabel.setText("node edited successfully.");
+    }
+  }
+
+  @FXML
+  public void deleteNode() {
+    String tempID = enterNodeID.getText();
+    if (tempID.equals(""))
+      errorLabel.setText("Missing Node ID.");
+    else{
+      if (App.getInstance().graph.deleteNode(tempID).equals(tempID))
+          errorLabel.setText("Node does not exists.");
+      else {
+        update();
+        errorLabel.setText("node deleted successfully.");
+      }
+    }
+  }
+
+  private boolean checkTextBoxesErrorCoordiantes() {
+    try {
+      Integer.parseInt(enterXCoo.getText());
+    } catch (NumberFormatException e) {
+      errorLabel.setText("x-coordinate is not a valid number");
+      return true;
+    }
+    try {
+      Integer.parseInt(enterYCoo.getText());
+    } catch (NumberFormatException e) {
+      errorLabel.setText("y-coordinate is not a valid number");
+      return true;
+    }
+
+    return false;
+  }
+
+  private boolean checkTextBoxesErrorCoordiantesEmpty() {
+    if (enterXCoo.getText().equals("")) {
+      errorLabel.setText("Missing x-coordinate.");
+      return true;
+    }
+    if (enterYCoo.getText().equals("")) {
+      errorLabel.setText("Missing y-coordinate.");
+      return true;
+    }
+    return false;
+  }
 
   @FXML
   public void buttonPressMain() throws Exception {
 
     Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/MainPage.fxml"));
-    App.getPrimaryStage().getScene().setRoot(root);
-  }
-
-  @FXML
-  public void buttonPressNode() throws Exception {
-
-    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/EditNodeLily.fxml"));
     App.getPrimaryStage().getScene().setRoot(root);
   }
 
@@ -69,5 +150,4 @@ public class NodeController {
     Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/AStarTyler.fxml"));
     App.getPrimaryStage().getScene().setRoot(root);
   }
-
 }
