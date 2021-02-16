645db1f1251c3d9d6b36b9992711aa3797ca0574
Kohmei Kadoya <kkadoya@wpi.edu>
2021-02-16 12:57:40 +0900

    Fixed tab navigation for all UI pages
    Needed to add button callback functions to all scenes. Didn't do nick's because it I did it on his page

--- src/main/java/edu/wpi/u/controllers/NodeController.java
+++ src/main/java/edu/wpi/u/controllers/NodeController.java
@@ -1,6 +1,9 @@
 package edu.wpi.u.controllers;
 
+import edu.wpi.u.App;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 
 public class NodeController {
@@ -38,4 +41,33 @@ public class NodeController {
 
   @FXML
   public void deleteNode() {}
+
+  @FXML
+  public void buttonPressMain() throws Exception {
+
+    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/MainPage.fxml"));
+    App.getPrimaryStage().getScene().setRoot(root);
+  }
+
+  @FXML
+  public void buttonPressNode() throws Exception {
+
+    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/EditNodeLily.fxml"));
+    App.getPrimaryStage().getScene().setRoot(root);
+  }
+
+  @FXML
+  public void buttonPressEdge() throws Exception {
+
+    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/EditEdgeNick.fxml"));
+    App.getPrimaryStage().getScene().setRoot(root);
+  }
+
+  @FXML
+  public void buttonPressAS() throws Exception {
+
+    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/AStarTyler.fxml"));
+    App.getPrimaryStage().getScene().setRoot(root);
+  }
+
 }
