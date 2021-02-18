dedab128ffc9f801dc2efe31e2a0863b0cf09a98
Will Burke <wgburke@wpi.edu>
2021-02-17 19:24:52 -0500

    Added method deleteNodeTest() to run JUnit test for removeNode()

--- src/test/java/edu/wpi/u/GraphTest.java
+++ src/test/java/edu/wpi/u/GraphTest.java
@@ -107,4 +107,14 @@ public class GraphTest {
     ArrayList<Node> nodes = ExampleGraph().getAllNodes();
     assertTrue(FindNodeID(nodes, "Test"));
   }
+  @Test
+  @DisplayName("DeleteNodeTest")
+  public void deleteNodeTest(){
+    Node n1 = new Node("TestID", 1, 5);
+    ExampleGraph().addNode(n1);
+    ExampleGraph().removeNode("TestID");
+    ArrayList<Node> testList = ExampleGraph().getAllNodes();
+    assertFalse(FindNodeID(testList,"TestID"));
+  }
+
 }
