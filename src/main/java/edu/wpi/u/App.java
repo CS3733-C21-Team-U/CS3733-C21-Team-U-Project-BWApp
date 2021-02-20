7af02efd32d0c434ca8a1d8fe1be9c849144f7fc
Kohmei Kadoya <kkadoya@wpi.edu>
2021-02-20 20:07:23 +0900

    Cleaning up things here and there for 1080, 1920

--- src/main/java/edu/wpi/u/App.java
+++ src/main/java/edu/wpi/u/App.java
@@ -4,9 +4,11 @@ import edu.wpi.u.models.DatabaseManager;
 import edu.wpi.u.models.GraphService;
 import edu.wpi.u.models.RequestService;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,18 +18,32 @@ public class App extends Application {
   // Allows for globally accessible instance of app to allow instance based editing
   // (separate info per open application)
   // Can be accessed by all controllers and classes by calling App.getInstance();
+  public static App app_instance = null;
+
+  private static Stage primaryStage;
+  // We only ever have one primary stage, each time we switch scenes, we swap this out
+
   public static GraphService graphService = new GraphService();
   public static RequestService requestService = new RequestService();
 
   public String DrawerState;
+
+  public App(){
+    System.out.println("App constructor");
+  }
+
+  public static App getInstance(){
+    if(app_instance == null){
+      app_instance = new App();
+    }
+    return app_instance;
+  }
 
   @Override
   public void init() {
     log.info("Starting Up");
   }
 
-  private static Stage primaryStage; // This is a static variable!!
-  // We only ever have one primary stage, each time we switch scenes, we swap this out
 
   public static Stage getPrimaryStage() {
     return primaryStage;
@@ -44,13 +60,22 @@ public class App extends Application {
     Parent root = FXMLLoader.load(getClass().getResource("views/NewMainPage.fxml"));
     Scene scene = new Scene(root);
     App.primaryStage.setScene(scene);
+    App.primaryStage.setFullScreen(true);
     App.primaryStage.show();
+
+    App.primaryStage.getScene().setOnKeyPressed(e -> {
+      if (e.getCode() == KeyCode.ESCAPE) {
+        System.out.println("Escape button pressed, exiting");
+        App.getInstance().stop();
+      }
+    });
   }
 
-
-  @Override
   public void stop() {
     System.out.println("Shutting Down");
     graphService.saveAndExitDB();
+    Stage stage = (Stage) App.primaryStage.getScene().getWindow();
+    stage.close();
   }
+
 }
