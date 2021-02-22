e52af73faee88cfa7ce6a109239dc060ba3ebe29
tyler <tasanderville@wpi.edu>
2021-02-21 23:28:16 -0600

    newrequestpage

--- src/main/java/edu/wpi/u/controllers/NewRequestController.java
+++ src/main/java/edu/wpi/u/controllers/NewRequestController.java
@@ -1,4 +1,17 @@
 package edu.wpi.u.controllers;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
 public class NewRequestController {
+
+    @FXML
+    public JFXTextField titleTextField;
+    @FXML
+    public JFXTextField descriptionTextField;
+    @FXML
+    public JFXTextField nodeTextField;
+    @FXML
+    public JFXTextField assigneeTextField;
+
 }
