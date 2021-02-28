c809af50d645c5d9aaf643c4c84e8085b895fb17
Will Burke <wgburke@wpi.edu>
2021-02-28 16:05:56 -0500

    Added minor change to fxml

--- src/main/java/edu/wpi/u/controllers/NodeContextMenuController.java
+++ src/main/java/edu/wpi/u/controllers/NodeContextMenuController.java
@@ -2,15 +2,23 @@ package edu.wpi.u.controllers;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import edu.wpi.u.App;
+import edu.wpi.u.algorithms.Node;
 import javafx.fxml.FXML;
 
 public class NodeContextMenuController {
+    Node node1;
+
     @FXML
     JFXTextField longNameText;
     @FXML
     JFXTextField shortNameText;
     @FXML
     JFXComboBox nodeTypeDrop;
+    @FXML
+    public void handleSaveButton(){
+        // App.mapService.updateNode() TODO: uppdate updateNode function
+    }
 
 
 
