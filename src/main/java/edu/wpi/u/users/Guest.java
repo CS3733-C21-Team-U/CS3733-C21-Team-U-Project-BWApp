90605bffd6041e16e1d340642d2210c5b880d440
Neville Ingram <nlingram@wpi.edu>
2021-02-28 20:54:36 -0500

    Added appointmentTime field to Guest

--- src/main/java/edu/wpi/u/users/Guest.java
+++ src/main/java/edu/wpi/u/users/Guest.java
@@ -1,9 +1,11 @@
 package edu.wpi.u.users;
 
+import java.sql.Time;
 import java.util.Date;
 
 public class Guest extends User{
     protected Date appointmentDate;
+    protected String appointmentTime;
     //TODO: provider name, appointment date/time, recommended self park location, way to save where they park their vehicle
     //TODO: Link to radiology and blood
     //guestID varchar(50) not null, name varchar(50), userName varchar(100), password varchar(100), email varchar(250), type varchar(50), phonenumber varchar(100), deleted boolean, appointmentDate date, primary key(guestID))";
