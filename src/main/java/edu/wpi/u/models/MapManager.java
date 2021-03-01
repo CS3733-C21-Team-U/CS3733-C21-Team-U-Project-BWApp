7f1f9eef3e732f01bdd16c94ff92d86329a67086
jacob <jbernard@wpi.edu>
2021-03-01 10:38:22 -0500

    Still trying to find the error

--- src/main/java/edu/wpi/u/models/MapManager.java
+++ src/main/java/edu/wpi/u/models/MapManager.java
@@ -24,12 +24,23 @@ public class MapManager {
    * @param _endNodeID
    */
   public void addEdge(String _edgeID, String _startNodeID, String _endNodeID) {
+    System.out.println("Edges are getting added to the list of allEdges. This edge is: " + _edgeID);
     Node _startNode = this.allNodes.get(_startNodeID);
+    System.out.println("Line 28 is good");
     Node _endNode = this.allNodes.get(_endNodeID);
+    System.out.println("Line 30 is good");
+    System.out.println(_startNode.getNodeID());
+    System.out.println(_startNode.getFloor());
+    System.out.println(_endNode.getNodeID());
+    System.out.println(_endNode.getFloor());
     Edge realEdge = new Edge(_edgeID, _startNode, _endNode);
+    System.out.println("Line 32 is good");
     this.allEdges.put(_edgeID, realEdge);
+    System.out.println("Line 34 is good");
     this.allNodes.put(_startNode.getNodeID(), _startNode);
+    System.out.println("Line 36 is good");
     this.allNodes.put(_endNode.getNodeID(), _endNode);
+    System.out.println("Line 38 is good");
   }
 
   /**
@@ -83,9 +94,7 @@ public class MapManager {
           String _LongName,
           String _ShortName,
           String _teamAssigned) {
-    Node n =
-            new Node(
-                    _nodeID, _xcoord, _ycoord, floor, _building, _nodeType, _LongName, _ShortName, _teamAssigned);
+    Node n = new Node(_nodeID, _xcoord, _ycoord, floor, _building, _nodeType, _LongName, _ShortName, _teamAssigned);
     allNodes.put(n.getNodeID(), n);
   }
 
