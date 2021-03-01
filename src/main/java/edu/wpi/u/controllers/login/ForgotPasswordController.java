297f9a1d56e9f50770ed3bcc8cdada8f45dbbda9
tyler <tasanderville@wpi.edu>
2021-02-28 18:50:37 -0600

    Added Forgot password page, pulled kohmeis branch in and working with that now

---
+++ src/main/java/edu/wpi/u/controllers/login/ForgotPasswordController.java
@@ -0,0 +1,15 @@
+package edu.wpi.u.controllers.login;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+public class ForgotPasswordController {
+
+    public JFXTextField contactInfoTextField;
+    public JFXButton sendButton;
+
+    public void handleSendButton(JFXTextField contactInfoTextField){
+
+
+    }
+}
