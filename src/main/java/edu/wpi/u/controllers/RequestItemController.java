cb1260238600d5f493e41516252babdc204f6643
Kaamil Lokhandwala <kslokhandwala@wpi.edu>
2021-03-02 01:25:07 -0500

    Fixed merge bugs

--- src/main/java/edu/wpi/u/controllers/RequestItemController.java
+++ src/main/java/edu/wpi/u/controllers/RequestItemController.java
@@ -40,4 +40,44 @@ public class RequestItemController {
         anchor.getChildren().clear();
         anchor.getChildren().add(root);
     }
+
+
+    /*
+    @FXML
+    public void handleExpandCollapseButton(){
+        if(isCollapsed) {
+            isCollapsed = false;
+            expandCollapseButton.setText("Collapse");
+            titleLabel.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            descriptionLabel.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            locationLabel.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        } else{
+            isCollapsed = true;
+            expandCollapseButton.setText("Expand");
+            titleLabel.setPrefHeight(45);
+            descriptionLabel.setPrefHeight(40);
+            locationLabel.setPrefHeight(40);
+
+        }
+    }
+
+    //Listener here for the global drawerstare variable
+    @FXML
+    public void handleChangeToEditRequest() throws Exception {
+        App.lastClickedRequestNumber = myID;
+        App.rightDrawerRoot.set("/edu/wpi/u/views/EditRequest.fxml");
+    }
+
+    @FXML
+    public void handleDeleteRequest() {
+        App.requestService.deleteRequest(myRequestID);
+        App.rightDrawerRoot.set("/edu/wpi/u/views/EditRequest.fxml"); //Fake - Just to refresh
+        App.rightDrawerRoot.set("/edu/wpi/u/views/Oldfxml/ViewRequest.fxml");
+    }
+
+    public void setTitle(String newTitle){
+        requestitleLabel.setText(newTitle);
+    }
+    public void setLocation(String newLocation){ locationLabel.setText(newLocation); }
+     */
 }
