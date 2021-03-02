48cb8079fcee85384bdb51d056b46ad8c4c05950
gearymich <Founders301_>
2021-03-02 00:34:39 -0500

    NewEditRequestController.java

--- src/main/java/edu/wpi/u/controllers/NewEditRequestController.java
+++ src/main/java/edu/wpi/u/controllers/NewEditRequestController.java
@@ -1,11 +1,13 @@
 package edu.wpi.u.controllers;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXChipView;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.u.App;
 import edu.wpi.u.algorithms.Node;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
@@ -19,36 +21,30 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 
-public class EditRequestController {
+public class NewEditRequestController {
 
-    @FXML public StackPane laundryStack;
-    @FXML public Pane makeEditLaundryPane;
-    @FXML public JFXTextField madeEditLaundryField;
-    @FXML public Pane makeEditSecurityPane;
-    @FXML public JFXTextField madeEditSecurityField;
-    @FXML public Pane makeEditMaintenancePane;
-    @FXML public JFXTextField madeEditMaintenanceField;
-    @FXML public JFXTextArea makeEditDescriptionField;
-    @FXML public JFXChipView makeEditLocationChipView;
-    @FXML public JFXChipView makeEditStaffChipView;
-    @FXML public JFXDatePicker makeEditDate2BCompleteDatePicker;
-    @FXML public JFXTextField makeEditTitleField;
-    @FXML public JFXCheckBox makeEditDateCheckBox;
-
-/*
+    //Generic Request Fields
     @FXML TextField editTitleField;
-    @FXML TextArea editDescripArea;
-    @FXML ListView showCurrentLocListView;
-    @FXML ChoiceBox editLocField;
-    @FXML Label editLocationErrorLabel;
-    @FXML TextField editTypeOfRequestField;
-    @FXML TextField editCompDateField;
-    @FXML CheckBox isCompleteCheckBox;
-    @FXML TextField editCreatorField;
-    @FXML ListView showCurrentPeopleListView;
-    @FXML TextField editPeopleField;
-    @FXML Label editPeopleErrorLabel;
 
+    @FXML TextArea editDescriptionField;
+
+    @FXML JFXDatePicker makeEditDate2BCompleteDatePicker;
+
+    @FXML JFXChipView makeEditLocationChipView;
+
+    @FXML JFXChipView makeEditStaffChipView;
+
+    //@FXML StackPane laundryStack;
+
+    //12 Specific Request Panes (3 currently)
+    @FXML Pane makeEditLaundryPane;
+
+    @FXML Pane makeEditSecurityPane;
+
+    @FXML Pane makeEditMaintenancePane;
+
+        @FXML JFXTextField madeEditMaintenanceField;
+        @FXML JFXTextField madeEditMaintenanceField1;
 
 
     private Request currRequest;
@@ -67,28 +63,30 @@ public class EditRequestController {
         currRequest = App.requestService.getRequests().get(App.lastClickedRequestNumber).getGenericRequest();
 
         editTitleField.setText(currRequest.getTitle());
-        editDescripArea.setText(currRequest.getDescription());
-        editTypeOfRequestField.setText(currRequest.getType());
+        editDescriptionField.setText(currRequest.getDescription());
+        //editTypeOfRequestField.setText(currRequest.getType()); //TODO: Type of request never changes, correct?
         //editCompDateField.setText(currRequest.getDateCompleted());
-        editCreatorField.setText("Admin");
+        //editCreatorField.setText("Admin");
         //For when creators switch
         //editCreatorField.setText(currRequest.getCreator());
 
-        for (int i = 0; i < currRequest.getLocation().size(); i++) {
-            showCurrentLocListView.getItems().add(currRequest.getLocation().get(i));
+
+        for (String l : currRequest.getLocation()) { //Locations
+            makeEditLocationChipView.getChips().add(l);
         }
 
-        for (int i = 0; i < currRequest.getAssignee().size(); i++) {
-            showCurrentPeopleListView.getItems().add(currRequest.getAssignee().get(i));
+        for (String a : currRequest.getAssignee()) { //Assignees
+            makeEditStaffChipView.getChips().add(a);
         }
 
-        ArrayList<Node> L = App.mapService.getNodes();//This gets the list of all the nodes
-        ArrayList<String> nodeIDs = new ArrayList<String>(); //Instantiating a new ArrayList for the NodeID's
-        for(Node N: L){//This fills up the new ArrayList<String> with the node ID's so we can display those
-            nodeIDs.add(N.getNodeID());
-        }
-        ObservableList<String> oList = FXCollections.observableList(nodeIDs);
-        editLocField.setItems(oList);
+        //TODO: What does this block do?
+//        ArrayList<Node> L = App.mapService.getNodes();//This gets the list of all the nodes
+//        ArrayList<String> nodeIDs = new ArrayList<String>(); //Instantiating a new ArrayList for the NodeID's
+//        for(Node N: L){//This fills up the new ArrayList<String> with the node ID's so we can display those
+//            nodeIDs.add(N.getNodeID());
+//        }
+//        ObservableList<String> oList = FXCollections.observableList(nodeIDs);
+//        editLocField.setItems(oList);
 
         FXMLLoader errorMessageLoader = new FXMLLoader(getClass().getResource("/edu/wpi/u/views/ErrorMessage.fxml"));
         AnchorPane error = errorMessageLoader.load();
@@ -101,6 +99,10 @@ public class EditRequestController {
         controller2 = errorMessageLoader2.getController();
         controller2.errorMessage.setText("Invalid People");
         errorDrawer2.setSidePane(error2);
+    }
+
+    private void handleSaveNewEditRequest() {
+        
     }
 
     private boolean isChecked() {
@@ -130,7 +132,7 @@ public class EditRequestController {
         if(!doesLocationExist()) {
             currRequest.getLocation().add(newLoc);
         } editLocationErrorLabel.setText("Location Already Listed.");
-    }
+    }*/
 
     public void handleAddLocation(){
         if (editLocField.getValue() == null) {
@@ -234,11 +236,5 @@ public class EditRequestController {
 
     public void handleErrorMessageClear2(){
         errorDrawer2.close();
-    }
-*/
-    public void HandleMakeEditCancelButton(ActionEvent actionEvent) {
-    }
-
-    public void handleSaveNewEditRequest(ActionEvent actionEvent) {
     }
 }
