50e2dacd40b3fb7d206ef22fce6ca6a29ec0cc37
Kohmei Kadoya <kkadoya@wpi.edu>
2021-03-04 01:00:04 +0900

    covid survey error checking

--- src/main/java/edu/wpi/u/controllers/login/COVIDController.java
+++ src/main/java/edu/wpi/u/controllers/login/COVIDController.java
@@ -7,6 +7,7 @@ import edu.wpi.u.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
@@ -18,6 +19,7 @@ public class COVIDController {
     public JFXCheckBox Q4CheckBox;
     public JFXCheckBox Q5CheckBox;
     public JFXButton submitSurveyButton;
+    public Label errorLabel;
 
 
     //  Intakes a set of checkbox values and will allow the user to go to the next page if the proper requirements are met
@@ -28,9 +30,19 @@ public class COVIDController {
      */
 
     public void handleCovidSurveyFill() throws IOException {
-            if (!Q1CheckBox.isSelected() &! Q2CheckBox.isSelected() &! Q3CheckBox.isSelected() &! Q4CheckBox.isSelected() & Q5CheckBox.isSelected()){
+        if (!Q1CheckBox.isSelected() &! Q2CheckBox.isSelected() &! Q3CheckBox.isSelected() &! Q4CheckBox.isSelected() & Q5CheckBox.isSelected()){
                 Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/UserLogin.fxml"));
                 App.getPrimaryStage().getScene().setRoot(root);
+        }else{
+            errorLabel.setText("We are sorry you are not feeling well or have been in contact with a COVID positive person. \n" +
+                    "Please call our office at 508-831-5520 between the hours of 9:00am and 4:30pm. \n" +
+                    "\n" +
+                    "Stay at home except for medical care. \n" +
+                    "Do not go to work, school, or public areas until told to do so by a Medical Provider. \n" +
+                    "\n" +
+                    "For more information on how to take care of yourself: CDC COVID Care Resource. \n" +
+                    "\n" +
+                    "If you have an emergency, please call campus police/5555 or 9-1-1.\n");
         }
     }
 }
