45f4b0cdf6e92c59865116001edcb5eabde495af
cvkittler <cvkittler@wpi.edu>
2021-03-02 23:50:37 -0500

    fixed edge undo/redo FINAL (?)

--- src/main/java/edu/wpi/u/algorithms/MapEdit.java
+++ src/main/java/edu/wpi/u/algorithms/MapEdit.java
@@ -131,7 +131,7 @@ public class MapEdit {
             this.type = "EDGE";
             this.edit = EditTypes.ADD;
             App.mapService.addEdge(start_node + "_" + end_node, start_node, end_node, permissions);
-            this.ID = start_node + "_" + end_node + "0";
+            this.ID = start_node + "_" + end_node;
         }catch (Exception e){
             throw e;
         }
