47fb0eaa67dab2ea85e2953b6eef4fd4df812171
Kohmei Kadoya <kkadoya@wpi.edu>
2021-03-03 09:41:14 +0900

    new 2FA Screen

---
+++ src/main/java/edu/wpi/u/controllers/login/Enter2FATokenController.java
@@ -0,0 +1,17 @@
+package edu.wpi.u.controllers.login;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
+public class Enter2FATokenController {
+    public JFXButton loginButton;
+
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    public void handleForgotPassword(ActionEvent actionEvent) {
+    }
+
+    public void handleGoBack(ActionEvent actionEvent) {
+    }
+}
