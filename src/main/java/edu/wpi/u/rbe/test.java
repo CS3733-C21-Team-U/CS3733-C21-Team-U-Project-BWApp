092a09926301ef6e067fb9691ee2d418dc2453c3
TianyangGao <1016498030@qq.com>
2021-03-06 16:51:07 -0500

    test example code
    need to fix serialport

--- src/main/java/edu/wpi/u/rbe/test.java
+++ src/main/java/edu/wpi/u/rbe/test.java
@@ -1,16 +1,19 @@
 package edu.wpi.u.rbe;
 
 import com.fazecast.jSerialComm.*;
+
 import javax.swing.*;
+
 
 
 public class test {
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        JSlider silder = new JSlider();
-        silder.setMaximum(1023);
-        window.add(silder);
+        JSlider slider = new JSlider();
+        slider.setMaximum(1023);
+        window.add(slider);
         window.pack();
         window.setVisible(true);
+        SerialPort.getCommPorts();
     }
 }
