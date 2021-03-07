b14da2ba7597953476551c61245140904a3802c8
TianyangGao <1016498030@qq.com>
2021-03-06 21:59:42 -0500

    Serial Port should work. Need someone test it on esp 32

--- src/main/java/edu/wpi/u/rbe/test.java
+++ src/main/java/edu/wpi/u/rbe/test.java
@@ -1,19 +1,45 @@
 package edu.wpi.u.rbe;
 
-import com.fazecast.jSerialComm.*;
+import com.fazecast.jSerialComm.SerialPort;
 
 import javax.swing.*;
-
+import java.util.Scanner;
 
 
 public class test {
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        JSlider slider = new JSlider();
+        JSlider slider = new JSlider();  // depend on what we connect on arduino
         slider.setMaximum(1023);
-        window.add(slider);
+        window.add(slider);  // add the device
         window.pack();
         window.setVisible(true);
         SerialPort.getCommPorts();
+
+        SerialPort ports[] = SerialPort.getCommPorts();
+
+        System.out.println("Select a port :");
+        int i = 1;
+        for (SerialPort port : ports) {
+            System.out.println(i++ + ", " + port.getSystemPortName());
+        }
+        Scanner s = new Scanner(System.in);  //conncet board
+        int chosenPort = s.nextInt();
+
+        SerialPort port = ports[chosenPort - 1];
+        if (port.openPort()) {
+            System.out.println("Successfully opened the port");
+        } else {
+            System.out.println("Fail to open the port");
+            return;
+        }
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0 ,0); //set timeout  necessary for windows
+
+        Scanner data = new Scanner(port.getInputStream());
+        while(data.hasNextLine()){ // wait input from serialport from Arduino
+            System.out.println(data.nextLine());
+
+        }
+
     }
 }
