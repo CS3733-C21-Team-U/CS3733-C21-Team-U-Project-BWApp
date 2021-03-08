f23abd1eb45a27c7ff4d56e18a4e02cb88f0918a
TianyangGao <1016498030@qq.com>
2021-03-08 16:31:21 -0500

    all sub help pages can return main help page

--- src/main/java/edu/wpi/u/controllers/adminhelp/LoadAndSaveCSVHelpPage.java
+++ src/main/java/edu/wpi/u/controllers/adminhelp/LoadAndSaveCSVHelpPage.java
@@ -1,4 +1,18 @@
 package edu.wpi.u.controllers.adminhelp;
 
+import edu.wpi.u.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class LoadAndSaveCSVHelpPage {
+    public void handleBackToMainPageButton(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchor = (AnchorPane) App.tabPaneRoot.getSelectionModel().getSelectedItem().getContent();
+        Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/MainHelpPage.fxml"));
+        anchor.getChildren().clear();
+        anchor.getChildren().add(root);
+    }
 }
