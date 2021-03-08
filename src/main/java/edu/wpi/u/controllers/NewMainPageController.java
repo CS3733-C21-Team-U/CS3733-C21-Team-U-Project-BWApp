c7f4769515f5f1f3a5177d5961c51f50574e6564
TianyangGao <1016498030@qq.com>
2021-03-08 16:36:09 -0500

    delete the help page button on the top right corner of the main page

--- src/main/java/edu/wpi/u/controllers/NewMainPageController.java
+++ src/main/java/edu/wpi/u/controllers/NewMainPageController.java
@@ -265,41 +265,7 @@ public class NewMainPageController {
 //        }
     }
 
-    public void handleHelpPageButton(ActionEvent actionEvent) {
 
-        JFXDialogLayout content = new JFXDialogLayout();
-        Label header = new Label("Help Page");
-        Text text = new Text("This is the Main page.If there is an emergency situation, Please call 911");
-        header.getStyleClass().add("headline-2");
-        content.setHeading(header);
-        content.setBody(text);
-        content.getStyleClass().add("dialogue");
-        JFXDialog dialog = new JFXDialog(newMainPageStackPane, content, JFXDialog.DialogTransition.CENTER);
-        JFXButton button1 = new JFXButton("Cancel");
-        JFXButton button2 = new JFXButton("Proceed to Help Page");
-        button1.setOnAction(event -> dialog.close());
-            button2.setOnAction(event -> {
-                AnchorPane anchor = (AnchorPane) App.tabPaneRoot.getSelectionModel().getSelectedItem().getContent();
-                Parent root = null;
-                try {
-                    root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/MainHelpPage.fxml"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                anchor.getChildren().clear();
-                anchor.getChildren().add(root);
-                dialog.close();
-            });
-
-
-        button1.getStyleClass().add("button-text");
-        button2.getStyleClass().add("button-contained");
-        ArrayList<Node> actions = new ArrayList<>();
-        actions.add(button1);
-        actions.add(button2);
-        content.setActions(actions);
-        dialog.show();
-    }
     public void handleCollapseButton(ActionEvent actionEvent) {
 //        this.list2.expandedProperty().set(true);
     }
