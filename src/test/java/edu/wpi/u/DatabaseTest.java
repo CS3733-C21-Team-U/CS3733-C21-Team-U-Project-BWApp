493c40f26eb26108de466b7b860f302b0cef9a44
Olajumoke <olajumoke.jackson@gmail.com>
2021-03-08 00:08:15 -0500

    Finished request tests

--- src/test/java/edu/wpi/u/DatabaseTest.java
+++ src/test/java/edu/wpi/u/DatabaseTest.java
@@ -4,6 +4,7 @@ import edu.wpi.u.algorithms.Edge;
 import edu.wpi.u.algorithms.Node;
 import edu.wpi.u.database.Data;
 import edu.wpi.u.database.Database;
+import edu.wpi.u.database.RequestData;
 import edu.wpi.u.exceptions.InvalidEdgeException;
 import edu.wpi.u.models.*;
 import edu.wpi.u.requests.*;
@@ -37,10 +38,10 @@ public class DatabaseTest {
         roles.add(Role.ADMIN);
 
         // Nodes for testing
-        Node node1 = new Node("TESTNODE1", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT","U");
-        Node node2 = new Node("TESTNODE2", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT","U");
-        Node node3 = new Node("TESTNODE3", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT","U");
-        Node node4 = new Node("TESTNODE4", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT","U");
+        Node node1 = new Node("TESTNODE1", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT", "U");
+        Node node2 = new Node("TESTNODE2", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT", "U");
+        Node node3 = new Node("TESTNODE3", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT", "U");
+        Node node4 = new Node("TESTNODE4", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT", "U");
 
         // Edges for testing
         Edge edge1 = new Edge("TESTNODE1_TESTNODE2", node1, node2, roles);
@@ -71,26 +72,27 @@ public class DatabaseTest {
         result.setRequest(newRequest);
         result.setSpecificData(specificFields);
 
-        mapServiceTestStaging.addNode(node1.getNodeID(),node1.getCords()[0],node1.getCords()[1], node1.getFloor(),node1.getBuilding(),node1.getNodeType(), node1.getLongName(), node1.getShortName());
-        mapServiceTestStaging.addNode(node2.getNodeID(),node2.getCords()[0],node2.getCords()[1], node2.getFloor(),node2.getBuilding(),node2.getNodeType(), node2.getLongName(), node2.getShortName());
-        mapServiceTestStaging.addNode(node3.getNodeID(),node3.getCords()[0],node3.getCords()[1], node3.getFloor(),node3.getBuilding(),node3.getNodeType(), node3.getLongName(), node3.getShortName());
-        mapServiceTestStaging.addNode(node4.getNodeID(),node4.getCords()[0],node4.getCords()[1], node4.getFloor(),node4.getBuilding(),node4.getNodeType(), node4.getLongName(), node4.getShortName());
+        mapServiceTestStaging.addNode(node1.getNodeID(), node1.getCords()[0], node1.getCords()[1], node1.getFloor(), node1.getBuilding(), node1.getNodeType(), node1.getLongName(), node1.getShortName());
+        mapServiceTestStaging.addNode(node2.getNodeID(), node2.getCords()[0], node2.getCords()[1], node2.getFloor(), node2.getBuilding(), node2.getNodeType(), node2.getLongName(), node2.getShortName());
+        mapServiceTestStaging.addNode(node3.getNodeID(), node3.getCords()[0], node3.getCords()[1], node3.getFloor(), node3.getBuilding(), node3.getNodeType(), node3.getLongName(), node3.getShortName());
+        mapServiceTestStaging.addNode(node4.getNodeID(), node4.getCords()[0], node4.getCords()[1], node4.getFloor(), node4.getBuilding(), node4.getNodeType(), node4.getLongName(), node4.getShortName());
 
-        userServiceTest.addEmployee("PersonName1", "UserName1", "password1", "email1", Role.ADMIN, "1111111111", "TEST1",false);
-        userServiceTest.addEmployee("PersonName2", "UserName2", "password2", "email2", Role.MAINTENANCE, "2222222222", "TEST2",false);
-        userServiceTest.addEmployee("PersonName3", "UserName3", "password", "email3", Role.ADMIN, "3333333333", "TEST3",false);
+        userServiceTest.addEmployee("PersonName1", "UserName1", "password1", "email1", Role.ADMIN, "1111111111", "TEST1", false);
+        userServiceTest.addEmployee("PersonName2", "UserName2", "password2", "email2", Role.MAINTENANCE, "2222222222", "TEST2", false);
+        userServiceTest.addEmployee("PersonName3", "UserName3", "password", "email3", Role.ADMIN, "3333333333", "TEST3", false);
 
         requestServiceTest.addRequest(result);
         ArrayList<SpecificRequest> testRequests = requestServiceTest.getRd().loadActiveRequests();
 
     }
 
+
     @Test
     public void testNodeFromID() throws InvalidEdgeException {
-        Node node1 = new Node("TESTNODE1", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT","U");
+        Node node1 = new Node("TESTNODE1", 100, 100, "1", "Faulkner", "HALL", "LOOOOONG", "SHORT", "U");
         MapService mapServiceTest = new MapService(testURL);
-        mapServiceTest.addNode(node1.getNodeID(),node1.getCords()[0],node1.getCords()[1], node1.getFloor(),node1.getBuilding(),node1.getNodeType(), node1.getLongName(), node1.getShortName());
-        assertEquals(mapServiceTest.getNodeFromID(node1.getNodeID()).getNodeID(),node1.getNodeID());
+        mapServiceTest.addNode(node1.getNodeID(), node1.getCords()[0], node1.getCords()[1], node1.getFloor(), node1.getBuilding(), node1.getNodeType(), node1.getLongName(), node1.getShortName());
+        assertEquals(mapServiceTest.getNodeFromID(node1.getNodeID()).getNodeID(), node1.getNodeID());
     }
 
     @Test
@@ -101,36 +103,36 @@ public class DatabaseTest {
         mapServiceTest.addNodeWithID("UWALK00401", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
         mapServiceTest.addNodeWithID("UWALK00501", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
         mapServiceTest.addEdge("UWALK00401_UWALK00501", "UWALK00401", "UWALK00501", roles);
-        assertEquals(mapServiceTest.getEdgeFromID("UWALK00401_UWALK00501").getEdgeID(),"UWALK00401_UWALK00501");
+        assertEquals(mapServiceTest.getEdgeFromID("UWALK00401_UWALK00501").getEdgeID(), "UWALK00401_UWALK00501");
     }
 
     @Test
     public void testAddNode1() throws InvalidEdgeException {
         MapService mapServiceTest = new MapService(testURL);
         mapServiceTest.addNodeWithID("NODETEST3", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
-        assertEquals(mapServiceTest.getNodeFromID("NODETEST3").getNodeID(),"NODETEST3"); // Node was found in db/data structure
+        assertEquals(mapServiceTest.getNodeFromID("NODETEST3").getNodeID(), "NODETEST3"); // Node was found in db/data structure
     }
 
     @Test
     public void testAddNode2() throws InvalidEdgeException {
         MapService mapServiceTest = new MapService(testURL);
-        mapServiceTest.addNode(1,1,"1","Faulkner", "TEST","longname","shortname");
-        assertEquals(mapServiceTest.getNodeFromID("UTEST00101").getNodeID(),"UTEST00101"); // Node was found in db/data structure
+        mapServiceTest.addNode(1, 1, "1", "Faulkner", "TEST", "longname", "shortname");
+        assertEquals(mapServiceTest.getNodeFromID("UTEST00101").getNodeID(), "UTEST00101"); // Node was found in db/data structure
     }
 
     @Test
     public void testAddNodeWithID() throws InvalidEdgeException {
         MapService mapServiceTest = new MapService(testURL);
         mapServiceTest.addNodeWithID("NODETEST4", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
-        assertEquals(mapServiceTest.getNodeFromID("NODETEST4").getNodeID(),"NODETEST4"); // Node was found in db/data structure
+        assertEquals(mapServiceTest.getNodeFromID("NODETEST4").getNodeID(), "NODETEST4"); // Node was found in db/data structure
     }
 
     @Test
     public void testUpdateNode() throws InvalidEdgeException {
         MapService mapServiceTest = new MapService(testURL);
         mapServiceTest.addNodeWithID("NODETEST5", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
-        mapServiceTest.updateNode("NODETEST5", 5, 5,"WALK","testy", "testywesty");
-        assertEquals(mapServiceTest.getNodeFromID("NODETEST5").getCords()[0],5,0);
+        mapServiceTest.updateNode("NODETEST5", 5, 5, "WALK", "testy", "testywesty");
+        assertEquals(mapServiceTest.getNodeFromID("NODETEST5").getCords()[0], 5, 0);
     }
 
     @Test
@@ -148,7 +150,7 @@ public class DatabaseTest {
         mapServiceTest.addNodeWithID("NODETEST7", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
         mapServiceTest.addNodeWithID("NODETEST8", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
         mapServiceTest.addEdge("NODETEST7_NODETEST8", "NODETEST7", "NODETEST8", roles);
-        assertEquals(mapServiceTest.getEdgeFromID("NODETEST7_NODETEST8").getEdgeID(),"NODETEST7_NODETEST8");
+        assertEquals(mapServiceTest.getEdgeFromID("NODETEST7_NODETEST8").getEdgeID(), "NODETEST7_NODETEST8");
     }
 
     @Test
@@ -162,7 +164,7 @@ public class DatabaseTest {
         roles.remove(Role.ADMIN);
         roles.add(Role.MAINTENANCE);
         mapServiceTest.updateEdgePermissions("NODETEST9_NODETEST10", roles);
-        assertEquals(mapServiceTest.getEdgeFromID("NODETEST9_NODETEST10").getUserPermissions().get(0),Role.MAINTENANCE);
+        assertEquals(mapServiceTest.getEdgeFromID("NODETEST9_NODETEST10").getUserPermissions().get(0), Role.MAINTENANCE);
     }
 
     @Test
@@ -201,7 +203,7 @@ public class DatabaseTest {
         mapServiceTest.addEdge("NODETEST17_NODETEST18", "NODETEST17", "NODETEST18", roles);
         mapServiceTest.addEdge("NODETEST18_NODETEST19", "NODETEST18", "NODETEST19", roles);
         mapServiceTest.deleteEdge("NODETEST17_NODETEST18");
-        assertEquals(mapServiceTest.getEdges().get(0),"NODETEST18_NODETEST19");
+        assertEquals(mapServiceTest.getEdges().get(0), "NODETEST18_NODETEST19");
     }
 
     @Test
@@ -209,7 +211,7 @@ public class DatabaseTest {
         MapService mapServiceTest = new MapService(testURL);
         mapServiceTest.addNodeWithID("NODETEST20", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
         mapServiceTest.addNodeWithID("NODETEST21", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
-        assertEquals(mapServiceTest.getNodes().get(0),"NODETEST20");
+        assertEquals(mapServiceTest.getNodes().get(0), "NODETEST20");
     }
 
     @Test
@@ -222,9 +224,103 @@ public class DatabaseTest {
         mapServiceTest.addNodeWithID("NODETEST24", 1, 1, "1", "Faulkner", "WALK", "Long", "Short");
         mapServiceTest.addEdge("NODETEST22_NODETEST23", "NODETEST22", "NODETEST23", roles);
         mapServiceTest.addEdge("NODETEST23_NODETEST24", "NODETEST23", "NODETEST24", roles);
-        assertEquals(mapServiceTest.getEdges().get(1).getEdgeID(),"NODETEST23_NODETEST24");
+        assertEquals(mapServiceTest.getEdges().get(1).getEdgeID(), "NODETEST23_NODETEST24");
     }
 
+    @Test
+    public void testAddRequest() {
+        RequestService requestServiceTest = new RequestService(testURL);
+        Date testDate = new GregorianCalendar(2022, 1, 1).getTime();
+        ArrayList<String> staffList = new ArrayList<String>();
+        staffList.add("PersonName1");
+        Comment testComment = new Comment("Comment 1", "This comment is for testing purposes", "PersonName1", CommentType.PRIMARY, new Timestamp(testDate.getTime()));
+        ArrayList<String> locations = new ArrayList<String>();
+        locations.add("TEST1");
+        ArrayList<String> specificFields = new ArrayList<>();
+        specificFields.add("Machine 1");
+        specificFields.add("High"); // Is this a valid string for priority??
+        SpecificRequest result = new RequestFactory().makeRequest("Maintenance");
+        Request newRequest = new Request("TestRequest1", new Timestamp(System.currentTimeMillis()), locations, staffList, testComment);
+        result.setRequest(newRequest);
+        result.setSpecificData(specificFields);
+        requestServiceTest.addRequest(result);
+        assertEquals(requestServiceTest.getRequests().get(0).getType(), result.getType());
+    }
 
+    @Test
+    public void testUpdateRequest() {
+        RequestService requestServiceTest = new RequestService(testURL);
+        Date testDate = new GregorianCalendar(2022, 1, 1).getTime();
+        ArrayList<String> staffList = new ArrayList<String>();
+        staffList.add("PersonName1");
+        Comment testComment = new Comment("Comment 1", "This comment is for testing purposes", "PersonName1", CommentType.PRIMARY, new Timestamp(testDate.getTime()));
+        ArrayList<String> locations = new ArrayList<String>();
+        locations.add("TEST1");
+        locations.add("TEST2");
+        ArrayList<String> specificFields = new ArrayList<>();
+        specificFields.add("Machine 1");
+        specificFields.add("High"); // Is this a valid string for priority??
+        SpecificRequest result = new RequestFactory().makeRequest("Maintenance");
+        Request newRequest = new Request("TestRequest1", new Timestamp(System.currentTimeMillis()), locations, staffList, testComment);
+        result.setRequest(newRequest);
+        result.setSpecificData(specificFields);
+        requestServiceTest.addRequest(result);
+        Comment testComment2 = new Comment("Comment 2", "This comment is for testing purposes", "PersonName1", CommentType.PRIMARY, new Timestamp(testDate.getTime()));
+        Request newRequest2 = new Request("TestRequest1", new Timestamp(System.currentTimeMillis()), locations, staffList, testComment2);
+        result.setRequest(newRequest2);
+        requestServiceTest.updateRequest(result);
+        assertEquals(result.getGenericRequest().getPrimaryComment(), testComment2);
+    }
 
+    @Test
+    public void testResolveRequest() {
+        RequestService requestServiceTest = new RequestService(testURL);
+        Date testDate = new GregorianCalendar(2022, 1, 1).getTime();
+        ArrayList<String> staffList = new ArrayList<String>();
+        staffList.add("PersonName1");
+        Comment testComment = new Comment("Comment 1", "This comment is for testing purposes", "PersonName1", CommentType.PRIMARY, new Timestamp(testDate.getTime()));
+        ArrayList<String> locations = new ArrayList<String>();
+        ArrayList<String> specificFields = new ArrayList<>();
+        specificFields.add("Machine 1");
+        specificFields.add("High"); // Is this a valid string for priority??
+        SpecificRequest result = new RequestFactory().makeRequest("Maintenance");
+        Request newRequest = new Request("TestRequest1", new Timestamp(System.currentTimeMillis()), locations, staffList, testComment);
+        result.setRequest(newRequest);
+        result.setSpecificData(specificFields);
+        requestServiceTest.addRequest(result);
+        Comment testComment2 = new Comment("Comment 2", "XX", "PersonName1", CommentType.PRIMARY, new Timestamp(testDate.getTime()));
+        requestServiceTest.resolveRequest(result, testComment2);
+        ArrayList <Comment> listOfComments = new ArrayList<>();
+        listOfComments.add(testComment);
+        listOfComments.add(testComment2);
+        ArrayList <Comment> requestComments = newRequest.getComments();
+        assertEquals(requestComments, listOfComments );
+    }
+
+    @Test
+    public void testGetRequest() {
+        RequestService requestServiceTest = new RequestService(testURL);
+        Date testDate = new GregorianCalendar(2022, 1, 1).getTime();
+        ArrayList<String> staffList = new ArrayList<String>();
+        staffList.add("PersonName1");
+        Comment testComment = new Comment("Comment 1", "This comment is for testing purposes", "PersonName1", CommentType.PRIMARY, new Timestamp(testDate.getTime()));
+        ArrayList<String> locations = new ArrayList<String>();
+        ArrayList<String> specificFields = new ArrayList<>();
+        specificFields.add("Machine 1");
+        specificFields.add("High"); // Is this a valid string for priority??
+        SpecificRequest result = new RequestFactory().makeRequest("Maintenance");
+        SpecificRequest result2 = new RequestFactory().makeRequest("Language");
+        Request newRequest = new Request("TestRequest1", new Timestamp(System.currentTimeMillis()), locations, staffList, testComment);
+        Request newRequest2 = new Request("TestRequest2", new Timestamp(System.currentTimeMillis()), locations, staffList, testComment);
+        result.setRequest(newRequest);
+        result2.setRequest(newRequest2);
+        result.setSpecificData(specificFields);
+        result2.setSpecificData(specificFields);;
+        ArrayList<SpecificRequest> testRequests = new ArrayList<>();
+        testRequests.add(result);
+        testRequests.add(result2);
+        requestServiceTest.addRequest(result);
+        requestServiceTest.addRequest(result2);
+        assertEquals(requestServiceTest.getRequests(), testRequests);
+    }
 }
