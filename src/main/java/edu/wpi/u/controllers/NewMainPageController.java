57a6893d33644316e30c9491bd832eb93b8a48b0
TianyangGao <1016498030@qq.com>
2021-03-09 03:49:13 -0500

    create admin new main help page
    seperate admin and general user(help page)
    
    delete dialog for those buttons in help page
    
    finish the log in help page ( add pictures and contents)

--- src/main/java/edu/wpi/u/controllers/NewMainPageController.java
+++ src/main/java/edu/wpi/u/controllers/NewMainPageController.java
@@ -34,6 +34,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
+
 import static edu.wpi.u.users.Role.ADMIN;
 
 public class NewMainPageController {
@@ -61,6 +62,8 @@ public class NewMainPageController {
     public JFXListView list2;
     public JFXButton expandButton;
     public JFXButton collapseButton;
+    public Tab HelpMainPageTab;
+    public Tab AdminHelpMainPageTab;
 
 
     AnchorPane rightServiceRequestPane;
@@ -70,6 +73,7 @@ public class NewMainPageController {
 
 
     public void initialize() throws IOException {
+
 
 
 //        validationFeild
@@ -137,12 +141,20 @@ public class NewMainPageController {
             adminTab1.setDisable(false);
             adminTab2.setStyle("-fx-opacity: 1");
             adminTab2.setDisable(false);
+            HelpMainPageTab.setDisable(true);
+            HelpMainPageTab.setStyle("-fx-opacity: 0");
+            AdminHelpMainPageTab.setDisable(false);
+            AdminHelpMainPageTab.setStyle("-fx-opacity: 1");
         }
         else if(!(App.userService.getActiveUser().getType() ==  ADMIN)){
             adminTab1.setStyle("-fx-opacity: 0");
             adminTab1.setDisable(true);
             adminTab2.setStyle("-fx-opacity: 0");
             adminTab2.setDisable(true);
+            HelpMainPageTab.setDisable(false);
+            HelpMainPageTab.setStyle("-fx-opacity: 1");
+            AdminHelpMainPageTab.setDisable(true);
+            AdminHelpMainPageTab.setStyle("-fx-opacity: 0");
         }
 
 //        chipView.getChips().addListener((new ListChangeListener<String>(){
