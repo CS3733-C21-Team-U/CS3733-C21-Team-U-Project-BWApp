715a957a4d17bb9abc61ff543cbed5613adfe9d0
gearymich <gearymich@gmail.com>
2021-03-09 19:17:49 -0500

    Handling for going thru both main entrances done

--- src/main/java/edu/wpi/u/controllers/pathfinding/FloatingPathfindingPaneController.java
+++ src/main/java/edu/wpi/u/controllers/pathfinding/FloatingPathfindingPaneController.java
@@ -130,14 +130,23 @@ public class FloatingPathfindingPaneController {
 
                 else if(eNode.getLongName().equals("Admitting")) {
                     if(sNode.getLongName().equals("Admitting")) {}
-                    else { stepHBoxContainer = createDirectionBox("Go Through Admitting", "");
+                    else { stepHBoxContainer = createDirectionBox("Go Through Admitting", "M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1" +
+                            " .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm6 12H6v-1.4c0-2 4-3.1 6-3.1s6 1.1 6 3.1V19z");
                     textDirectionContainer.getChildren().add(stepHBoxContainer);
+                    }}
+
+                else if(eNode.getLongName().equals("Atrium Main Lobby")) {
+                    if(sNode.getLongName().equals("Atrium Main Lobby")) {}
+                    else { stepHBoxContainer = createDirectionBox("Go Through Atrium Main Lobby", "M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1" +
+                            " .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm6 12H6v-1.4c0-2 4-3.1 6-3.1s6 1.1 6 3.1V19z");
+                        textDirectionContainer.getChildren().add(stepHBoxContainer);
                     }}
 
                 //else if(sNode.getLongName().equals("Admitting") || bNode.getLongName().equals("Admitting")) { }
 
                 else if(angleDifferance > 22.5 && angleDifferance < 337.5) { //if turn is found on current node
-                    if(lastTurnNode != null && !(sNode.getLongName().equals("Admitting")) && !(eNode.getLongName().equals("Admitting"))) {
+                    if(lastTurnNode != null && !(sNode.getLongName().equals("Admitting")) && !(bNode.getLongName().equals("Admitting"))
+                            && !(sNode.getLongName().equals("Atrium Main Lobby")) && !(bNode.getLongName().equals("Atrium Main Lobby"))) {
                         stepHBoxContainer = createDirectionBox("Continue straight for " + distAggregate(lastTurnNode, sNode) + " feet", "M5,9l1.41,1.41L11,5.83V22H13V5.83l4.59,4.59L19,9l-7-7L5,9z");
                         textDirectionContainer.getChildren().add(stepHBoxContainer);
                     }
@@ -163,7 +172,7 @@ public class FloatingPathfindingPaneController {
                 else if((sNode.getNodeType().equals("WALK") && eNode.getNodeType().equals("WALK"))) {/*Walkway*/ }
 
                 else {
-                    if(!(sNode.getLongName().equals("Admitting")) && !(eNode.getLongName().equals("Admitting"))) {
+                    if(!(sNode.getLongName().equals("Admitting")) && !(eNode.getLongName().equals("Admitting")) && !(bNode.getLongName().equals("Admitting"))) {
                     stepHBoxContainer = createDirectionBox("Continue straight for " + distAggregate(lastTurnNode, eNode) + " feet", "M5,9l1.41,1.41L11,5.83V22H13V5.83l4.59,4.59L19,9l-7-7L5,9z");
                     textDirectionContainer.getChildren().add(stepHBoxContainer);
                     //lastTurnNode = eNode;
