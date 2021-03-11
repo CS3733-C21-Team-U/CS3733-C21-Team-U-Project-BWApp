0e6529e99754bbdb7a890a663db2cd9ec8c1b1a2
Neville Ingram <nlingram@wpi.edu>
2021-03-11 16:47:32 -0500

    General cleanup

--- src/main/java/edu/wpi/u/Main.java
+++ src/main/java/edu/wpi/u/Main.java
@@ -4,8 +4,6 @@ import com.sun.javafx.application.LauncherImpl;
 
 public class Main {
   public static void main(String[] args) {
-    //System.setProperty("javafx.preloader", FirstPreloader.class.getCanonicalName());
-    //LauncherImpl.launchApplication(App.class, FirstPreloader.class, args);
     App.launch(App.class, args);
   }
 }
