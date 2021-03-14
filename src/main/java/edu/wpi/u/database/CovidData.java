de675d90069d9a8b793fd9fdf15f68d5184afac1
Olajumoke <olajumoke.jackson@gmail.com>
2021-03-13 23:12:56 -0500

    addDataPoint can add survey results into covidSurveyResults table

--- src/main/java/edu/wpi/u/database/CovidData.java
+++ src/main/java/edu/wpi/u/database/CovidData.java
@@ -1,11 +1,20 @@
 package edu.wpi.u.database;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+import soot.Local;
+
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
-public class CovidData extends Data{
+public class CovidData{
     Boolean result;
     // Wherever survey is: App.covidService.addSurveyResults(true, date);
 
@@ -19,22 +28,35 @@ public class CovidData extends Data{
 
     public void insertData(boolean result){
         String str;
-        Date todayDate = Date.valueOf(String.valueOf(java.time.LocalDateTime.now()));
-        todayDate.toLocalDate();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate newDate = LocalDate.now();
+        String todayDate = (newDate.format(dtf));
+        final java.sql.Date sqlDate = java.sql.Date.valueOf(todayDate);
+        str = "insert into covidSurveyResult(symptomatic, nonsymptomatic, dateOfResults) values ( ?, ?, ?)";
+        //System.out.println(todayDate);;
         if(result) {
-            str = "insert into covidSurveyResult(symptomatic, nonsymptomatic," +  todayDate + ") values ( true, false ," + todayDate + ")";
+            try{
+                PreparedStatement ps = conn.prepareStatement(str);
+                ps.setBoolean(1, true);
+                ps.setBoolean(2, false);
+                ps.setObject(3, sqlDate);
+                ps.execute();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                throwables.getCause();
+            }
         }
         else{
-            System.out.println("Reached Here");
-            str = "insert into covidSurveyResult(symptomatic, nonsymptomatic," +  todayDate + ") values (false , true ," + todayDate + ")";
-        }
-        try{
-            PreparedStatement ps = conn.prepareStatement(str);
-            ps.execute();
-        } catch (SQLException throwables) {
-            System.out.println("Reached Exception");
-            throwables.printStackTrace();
-            throwables.getCause();
+            try{
+                PreparedStatement ps = conn.prepareStatement(str);
+                ps.setBoolean(1, false);
+                ps.setBoolean(2, true);
+                ps.setObject(3, sqlDate);
+                ps.execute();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                throwables.getCause();
+            }
         }
     }
 
@@ -43,7 +65,20 @@ public class CovidData extends Data{
      * @return - Number of symptomatic surveys
      */
     public int getNumSymptomaticFromDate(Date targetDate){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String todayDate = (targetDate.toString().format(String.valueOf(dtf)));
+        final java.sql.Date sqlDate = java.sql.Date.valueOf(todayDate);
+        String str = "select count(symptomatic) from covidSurveyResult where dateOfResults = " + sqlDate ;
+        try{
+            PreparedStatement ps = conn.prepareStatement(str);
+            ResultSet rset = ps.executeQuery();
+            rset.next();
+            return rset.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return 0;
+
     }
 
     /**
@@ -51,6 +86,16 @@ public class CovidData extends Data{
      * @return - Number of non-symptomatic surveys
      */
     public int getNumNonSymptomaticFromDate(Date targetDate){
+
+        String str = "select count(nonsymptomatic) from covidSurveyResult where dateOfResults = " + targetDate ;
+        try{
+            PreparedStatement ps = conn.prepareStatement(str);
+            ResultSet rset = ps.executeQuery();
+            rset.next();
+            return rset.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return 0;
     }
 
@@ -62,6 +107,18 @@ public class CovidData extends Data{
         return 0;
     }
 
+/*
+String str = "select count(symptomatic) from covidSurveyResult where dateOfResults(day, -7, getDate())";
+        try{
+            PreparedStatement ps = conn.prepareStatement(str);
+            ResultSet rset = ps.executeQuery();
+            rset.next();
+            return rset.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return 0;
+ */
 
 
 }
