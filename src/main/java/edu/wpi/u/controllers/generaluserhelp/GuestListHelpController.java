710ee44742084a0d8f336243005decdda31d8449
TianyangGao <1016498030@qq.com>
2021-03-15 05:13:26 -0400

    fix the general and admin help user issue

--- src/main/java/edu/wpi/u/controllers/generaluserhelp/GuestListHelpController.java
+++ src/main/java/edu/wpi/u/controllers/generaluserhelp/GuestListHelpController.java
@@ -1,4 +1,8 @@
 package edu.wpi.u.controllers.generaluserhelp;
 
+import javafx.event.ActionEvent;
+
 public class GuestListHelpController {
+    public void handleBackToMainPageButton(ActionEvent actionEvent) {
+    }
 }
