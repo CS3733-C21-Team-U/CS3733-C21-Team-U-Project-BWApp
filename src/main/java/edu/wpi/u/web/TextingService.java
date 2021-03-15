34f2bca8e7702bff067d11f82e601e92380a4305
Neville Ingram <nlingram@wpi.edu>
2021-03-15 17:47:41 -0400

    Working sms setting, setup for formatting

--- src/main/java/edu/wpi/u/web/TextingService.java
+++ src/main/java/edu/wpi/u/web/TextingService.java
@@ -3,6 +3,7 @@ package edu.wpi.u.web;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import edu.wpi.u.requests.SpecificRequest;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.Objects;
@@ -16,8 +17,7 @@ public class TextingService {
     }
 
     public static void main(String[] args) {
-        //new TextingService();
-        //sendText("+19148394600", "Hello there test");
+
     }
 
     public void sendText(String to, String body){
@@ -25,6 +25,17 @@ public class TextingService {
                 .creator(new PhoneNumber(to),
                         new PhoneNumber(Objects.requireNonNull(dotenv.get("TWILIO_NUMBER"))),
                         body)
+                .create();
+    }
+    public void sendText(String to, SpecificRequest specificRequest){
+        Message message = Message
+                .creator(new PhoneNumber(to),
+                        new PhoneNumber(Objects.requireNonNull(dotenv.get("TWILIO_NUMBER"))),
+                        "New " + specificRequest.getType() + " request" +
+                        "Created by : " + specificRequest.getGenericRequest().getAuthor() +
+                        "Date Needed : " + specificRequest.getGenericRequest().getDateNeeded() +
+                        "Title : " + specificRequest.getGenericRequest().getTitle() +
+                        "Desscription : " + specificRequest.getGenericRequest().getDescription())
                 .create();
     }
 }
