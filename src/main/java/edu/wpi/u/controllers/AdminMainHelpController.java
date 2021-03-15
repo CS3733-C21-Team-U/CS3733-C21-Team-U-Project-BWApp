05c2651574f5f52005a36950a99b902b6f60628e
TianyangGao <1016498030@qq.com>
2021-03-15 17:42:14 -0400

    add my request to admin help page
    finish polishing pathfinding, my request help page

--- src/main/java/edu/wpi/u/controllers/AdminMainHelpController.java
+++ src/main/java/edu/wpi/u/controllers/AdminMainHelpController.java
@@ -174,4 +174,16 @@ public class AdminMainHelpController {
         anchor.getChildren().clear();
         anchor.getChildren().add(root);
     }
+
+    public void handleMyRequestButton(ActionEvent actionEvent) {
+        AnchorPane anchor = (AnchorPane) App.tabPaneRoot.getSelectionModel().getSelectedItem().getContent();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/edu/wpi/u/views/generaluserhelp/MyRequestHelpPage.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        anchor.getChildren().clear();
+        anchor.getChildren().add(root);
+    }
 }
